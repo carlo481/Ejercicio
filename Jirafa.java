b7d5c08f6b0a1cb6b286abef59422d6a2ab3778e
carlo481 <70033077+carlo481@users.noreply.github.com>
2020-09-05 15:52:35 -0600

    Jirafa
    Hola

--- Jirafa.java
+++ Jirafa.java
@@ -18,7 +18,46 @@ public class Jirafa {
         // TODO code application logic here
         
         System.out.println("Hola");
-        
+        public class Animal {
+ 
+    private String nombre;
+    private String tipo_alimentacion;
+    private int edad;
+
+    public Animal(String nombre, String tipo_alimentacion, int edad) {
+        this.nombre = nombre;
+        this.tipo_alimentacion = tipo_alimentacion;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo_alimentacion() {
+        return tipo_alimentacion;
+    }
+
+    public void setTipo_alimentacion(String tipo_alimentacion) {
+        this.tipo_alimentacion = tipo_alimentacion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Alimentarse");
+    }
     }
     
 }
